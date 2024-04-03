77d4fb5af8a6e8cc50f54780c3234995fba87dd8
tatendabakozw <tatendabakozw@gmail.com>
2024-04-03 22:29:07 +0200

    feature: new login page

--- apps/dashboard/src/pages/index.tsx
+++ apps/dashboard/src/pages/index.tsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import PrimaryButton from "../components/buttons/PrimaryButton";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import Image from "next/image";
+import CustomInput from "@/components/inputs/CustomInput";
 
 export function Index() {
   const [email, setEmail] = useState("");
@@ -32,52 +34,42 @@ export function Index() {
       <Head>
         <title>Dashboard - Login</title>
       </Head>
-      <div className="overflow-hidden relative min-h-screen grid items-center  w-full px-4">
-        
+      <div className="overflow-hidden relative min-h-screen grid pt-40 w-full px-4">
         <div className="max-w-sm mx-auto w-full flex flex-col space-y-6">
-          <p className="text-slate-900 text-lg font-semibold text-center">
-            Welcome Back
-          </p>
-          <div className="flex flex-col w-full space-y-2">
-            <label
-              htmlFor="email"
-              className="z-10 text-slate-700 text-sm font-semibold"
-            >
-              Email address
-            </label>
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="bg-white z-10 border border-slate-400 rounded p-2"
-            />
-          </div>
-          <div className="flex flex-col w-full space-y-2">
-            <div className="flex flex-row items-center justify-between">
-              <label
-                htmlFor="password"
-                className="z-10 text-slate-700 text-sm font-semibold"
-              >
-                Password
-              </label>
-              <Link
-                href={"/login"}
-                className="text-xs font-medium text-slate-700 text-center"
-              >
-                Forgot password?
-              </Link>
+          <div className="flex flex-row self-start">
+            <div className="w-16 h-12 relative rounded-lg">
+              <Image
+                src={"/images/velocity.svg"}
+                alt="logo icon"
+                layout="fill"
+                objectFit="cover"
+              />
             </div>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="bg-white z-10 border border-slate-400 rounded p-2"
-            />
           </div>
-          <PrimaryButton text="Sign in to account" onClick={login_user} />
+          {/* <BorderedHeading text="Welcome Back" /> */}
+          <p className="text-2xl heading-text text-left font-semibold">Sign in to your account</p>
+          <CustomInput
+            heading="Email Address"
+            value={email}
+            setValue={setEmail}
+            placeholder="Enter email address"
+          />
+          <CustomInput
+            heading="Password"
+            value={password}
+            setValue={setPassword}
+            placeholder="Password"
+          />
           <Link
             href={"/login"}
-            className="text-xs font-medium text-slate-700 text-center"
+            className="text-xs font-medium  main-link-text text-right"
+          >
+            Forgot password?
+          </Link>
+          <PrimaryButton text="Sign in to account" onClick={login_user} />
+          <Link
+            href={"/register"}
+            className="text-xs font-medium main-link-text text-center"
           >
             Don't have an account?
           </Link>
