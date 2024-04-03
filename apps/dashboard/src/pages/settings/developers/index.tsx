30eebca4ef8c044cc2b70d52c8135a6a68baad9b
tatendabakozw <tatendabakozw@gmail.com>
2024-04-03 09:19:36 +0200

    feaute: added settings sub nav

--- apps/dashboard/src/pages/settings/developers/index.tsx
+++ apps/dashboard/src/pages/settings/developers/index.tsx
@@ -1,16 +1,21 @@
-import DashboardLayout from '@/layouts/DashboardLayout'
-import React from 'react'
+import DevelopersNavigation from "@/components/navigation/DevelopersNavigation";
+import DashboardLayout from "@/layouts/DashboardLayout";
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const Developers = (props: Props) => {
   return (
     <DashboardLayout>
-        <div className="flex max-w-7xl w-full mx-auto">
-            alsjlss
+      <div className="max-w-7xl w-full mx-auto md:p-8 p-4 flex flex-col space-y-8">
+        <p className="heading-text text-3xl font-bold">Developers</p>
+        <DevelopersNavigation />
+        <div className="main-border p-4 rounded-lg justify-between">
+            <p className="heading-text font-bold">API Keys</p>
         </div>
+      </div>
     </DashboardLayout>
-  )
-}
+  );
+};
 
-export default Developers
+export default Developers;
