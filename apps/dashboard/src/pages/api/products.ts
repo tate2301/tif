04f92338c8323d4a413df53fc127d7e3e6f6a2a0
tatendabakozw <tatendabakozw@gmail.com>
2024-04-04 01:13:36 +0200

    fix: types defination on produts api route

--- apps/dashboard/src/pages/api/products.ts
+++ apps/dashboard/src/pages/api/products.ts
@@ -4,12 +4,22 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 // get a single produt
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  // create a product toute
   if (req.method === "POST") {
-    const { name, email } = req.body; // Assuming JSON data
-    // Process the data from req.body
-    res.status(200).json({ message: `Received data: ${name}, ${email}` });
+    // Handle POST request logic here
+    const { name, email } = req.body; // Assuming JSON data in body
+    // Process the data (e.g., save to database)
+    console.log("Received data:", { name, email });
+    res.status(201).json({ message: "Data created successfully!" });
+
+    // get a product route
+  } else if (req.method === "GET") {
+    // Handle GET request logic here
+    // You can potentially retrieve data from a database or perform other actions
+    const data = [{ message: "This is data from the GET request" }];
+    res.status(200).json(data);
   } else {
-    // Handle other HTTP methods (optional)
+    // Handle other unsupported HTTP methods (optional)
     res.status(405).json({ message: "Method Not Allowed" });
   }
 }
