1e1bd27bacb412e1c14945713db0cca6e3348247
tate2301 <thehalfstackdev@gmail.com>
2024-04-04 00:58:36 +0200

    feat: add middleware

--- apps/dashboard/src/pages/_middleware.ts
+++ apps/dashboard/src/pages/_middleware.ts
@@ -1,24 +1,34 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 
-export async function middleware(req, ev) {
-  const token = req ? req.cookies?.token : null;
+export async function middleware(req: NextRequest) {
+  // extract the token from the request headers
+  const token = req.headers.get("Authorization")?.replace("Bearer ", "");
 
-  let userId = null;
+  let user = null;
   if (token) {
-    // this is how we sign= jwt.sign(object,secretKey)
-    // now use the same secretKey to decode the token
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-    userId = decodedToken?.issuer;
+    const decodedToken = jwt.verify(token, "supersecret");
+    if (!decodedToken) return NextResponse.redirect("/login");
+
+    user = (
+      decodedToken as {
+        user: {
+          id: string;
+          email: string;
+          first_name: string;
+          last_name: string;
+        };
+      }
+    ).user.id;
   }
 
   const { pathname } = req.nextUrl;
-  // if user sends request to "/api/login", it has no token. so let the request pass
-  if (pathname.includes("/api/login") || userId) {
-    return NextResponse.next();
+
+  if (!token && pathname !== "/" && pathname !== "/register") {
+    return NextResponse.redirect("/");
   }
 
-  if (!token && pathname !== "/login") {
-    return NextResponse.redirect("/login");
+  if (pathname.includes("/") || pathname.includes("/register") || user) {
+    return NextResponse.next();
   }
 }
