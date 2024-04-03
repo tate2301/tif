8129f1b34d5d54b6ae73623f74657fab99065dfd
tatendabakozw <tatendabakozw@gmail.com>
2024-04-04 01:11:47 +0200

    fix: types defination on produts api route

--- apps/dashboard/src/pages/api/products.ts
+++ apps/dashboard/src/pages/api/products.ts
@@ -1,5 +1,17 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextRequest, res: NextResponse) {
-  return res.status(200).json({ message: "Hello from Next.js API!" });
+// cteate product
+
+// get a single produt
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    const { name, email } = req.body; // Assuming JSON data
+    // Process the data from req.body
+    res.status(200).json({ message: `Received data: ${name}, ${email}` });
+  } else {
+    // Handle other HTTP methods (optional)
+    res.status(405).json({ message: "Method Not Allowed" });
+  }
 }
+
+// get all products
