d3e2a52949294619cca13a7248a1d2a8010b6547
tatendabakozw <tatendabakozw@gmail.com>
2024-04-03 09:12:54 +0200

    feature: added settings page

--- apps/dashboard/src/components/navigation/Navbar.tsx
+++ apps/dashboard/src/components/navigation/Navbar.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable @typescript-eslint/ban-types */
-import React, { useState } from 'react';
-import ProjectsMenu from '../menus/ProjectsMenu';
-import ThemeToggler from '../buttons/ThemeToggle';
+import React, { useState } from "react";
+import ProjectsMenu from "../menus/ProjectsMenu";
+import ThemeToggler from "../buttons/ThemeToggle";
 
-import { useRouter } from 'next/router';
-import Link from 'next/link';
-import UserMenu from '../menus/UserMenu';
-import { data } from '../../utils/data';
+import { useRouter } from "next/router";
+import Link from "next/link";
+import UserMenu from "../menus/UserMenu";
+import { data } from "../../utils/data";
+import { Cog6ToothIcon, CogIcon } from "@heroicons/react/24/outline";
 
 type Props = {};
 
@@ -25,8 +26,8 @@ const Navbar = (props: Props) => {
               key={index}
               className={`${
                 pathname === item.location
-                  ? 'text-slate-900 dark:text-white font-bold'
-                  : 'main-link-text '
+                  ? "text-slate-900 dark:text-white font-bold"
+                  : "main-link-text "
               }  hover:font-semibold`}
             >
               {item.name}
@@ -35,6 +36,9 @@ const Navbar = (props: Props) => {
         </div>
         <div className="flex-1" />
         <ThemeToggler />
+        <Link href={'/settings/developers'} className=" transition-all cursor-pointer duration-100 main-link-text dark:hover:bg-slate-800 hover:bg-slate-100 p-1 rounded-full">
+          <Cog6ToothIcon height={20} width={20} />
+        </Link>
 
         <UserMenu />
       </div>
---
+++ apps/dashboard/src/pages/settings/developers/index.tsx
@@ -0,0 +1,16 @@
+import DashboardLayout from '@/layouts/DashboardLayout'
+import React from 'react'
+
+type Props = {}
+
+const Developers = (props: Props) => {
+  return (
+    <DashboardLayout>
+        <div className="flex max-w-7xl w-full mx-auto">
+            alsjlss
+        </div>
+    </DashboardLayout>
+  )
+}
+
+export default Developers
