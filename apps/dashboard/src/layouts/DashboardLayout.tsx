960f976cbcee5d7fc57d08839190410f699090cc
tatendabakozw <tatendabakozw@gmail.com>
2024-04-04 01:05:04 +0200

    featire: create payment link

--- apps/dashboard/src/layouts/DashboardLayout.tsx
+++ apps/dashboard/src/layouts/DashboardLayout.tsx
@@ -10,17 +10,21 @@ type Props = {
 };
 
 const DashboardLayout = (props: Props) => {
-  const { state } = useContext<any>(Store);
-  const { access_token } = state;
-  const router = useRouter();
+  // const { state } = useContext<any>(Store);
+  // const { access_token } = state;
+  // const router = useRouter();
 
-  console.log("user Info: ----- ", access_token);
+  // console.log("user Info: ----- ", access_token);
 
-  useEffect(() => {
-    if (!access_token) {
-      router.push("/");
-    }
-  }, [access_token]);
+  /*
+    TODO: protect all routes
+  */
+
+  // useEffect(() => {
+  //   if (!access_token) {
+  //     router.push("/");
+  //   }
+  // }, []);
 
   return (
     <>
